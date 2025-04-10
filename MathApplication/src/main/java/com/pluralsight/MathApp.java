aefc55c57f35a9d030cdc65790657ca1852c9170
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-10 14:37:31 -0700

    Attempting to add MathApp
    created a new java project named MathApplication

---
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+            //Q1:
+            int bobSalary = 45,000;
+            int garySalary = 45,500;
+            int highestSalary = Math.max(bobSalary, garySalary);
+        }
+
+    }
+}
