1cda1908745df504a617bc7b438264d7ab7a6f0c
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-10 15:30:47 -0700

    Added working MathApp class
    Deleted mathapp class to start over; then created a new one to start from scratch since I was confused on why it wasn't running.

--- MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -1,13 +1,17 @@
 package com.pluralsight;
 
 public class MathApp {
-    public static void main(String[] args) {
 
-            //Q1:
-            int bobSalary = 45,000;
-            int garySalary = 45,500;
-            int highestSalary = Math.max(bobSalary, garySalary);
-        }
+    //Q1:
+    double bobSalary = 45000;
+    double garySalary = 45500;
+    double highestSalary = Math.max(bobSalary, garySalary);
 
-    }
+    //Q2:
+    double carPrice = 25000.50;
+    double truckPrice = 34200.50;
+    double lowestPrice = Math.min(carPrice, truckPrice);
+
 }
+
+
--- MathApplication/src/main/java/org/example/Main.java
+++ MathApplication/src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Q1:
+        double bobSalary = 45000;
+        double garySalary = 45500;
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+        System.out.println("Bob's salary is: $" + bobSalary);
+        System.out.println("Gary's salary is: $" + garySalary);
+        System.out.println("The  highest salary is: $" + highestSalary);
+
+        System.out.println("");
+
+        //Q2:
+        double carPrice = 25000.50;
+        double truckPrice = 34200.50;
+        double lowestPrice = Math.min(carPrice, truckPrice);
+
+        System.out.println("A car's price is: $" + carPrice);
+        System.out.println("A truck's price is: $" + truckPrice);
+        System.out.println("The lowest price is: $" + lowestPrice);
     }
+
 }
+
+
