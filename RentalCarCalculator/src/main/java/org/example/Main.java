ba460d6bb8e68b6073e44dd5c42b17eaaaa71090
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-22 22:39:35 -0700

    Rental Car Calculator Exercise (Work in progress)
    -used getters/setters
    -created classes to have main call methods from it
    **Still need to make a lot of adjustments**

--- RentalCarCalculator/src/main/java/org/example/Main.java
+++ RentalCarCalculator/src/main/java/org/example/Main.java
@@ -1,10 +1,25 @@
 package org.example;
+import javax.print.MultiDocPrintService;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
 
         CostEstimate estimate = new CostEstimate();
         estimate.rentalInfo();
         estimate.calculations();
+
+
+//        //In class exercise: Week 2, Tuesday
+//        //String customer = "C1000";
+//        System.out.println("Enter customer code: ");
+//        String customerCode = read.nextLine();
+//
+//        if (customerCode.startsWith("C")){
+//            System.out.println("Good customer code!");
+//        }else{
+//            System.out.println("Customer code should start with a C");
+        }
     }
 }
